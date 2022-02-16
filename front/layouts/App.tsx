a2bc00a4a29caccae7d7532a02f1bd47f6b0f0d1
seongjunme <72444675+seongjunme@users.noreply.github.com>
2022-02-16 22:36:18 +0900

    Feature: 리액트 라우터 생성

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from '@pages/Login';
+import SignUp from '@pages/SignUp';
 
 const App = () => {
-  return <div>App ㅋㅋㅋ</div>;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/" element={<Navigate to="/login" />} />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ front/pages/Login/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Login = () => {
+  return <div>Loginasdfadfasd</div>;
+};
+
+export default Login;
