691b004b8923fea6b809968bf65cee966c47cb80
seongjunme <72444675+seongjunme@users.noreply.github.com>
2022-02-18 18:12:02 +0900

    Fix: 라우터 구현 변경
    - 로그인 상태가 아니면 login, signup 컴포넌트 렌더링 허용
    - 로그인 상태면 login, signup 컴포넌트 비허용

--- front/index.tsx
+++ front/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from '@layouts/App';
+
+const queryClient = new QueryClient();
 
 render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>,
   document.querySelector('#app'),
 );
--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import loadable from '@loadable/component';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import fetcher from '@utils/fetcher';
 
 const Login = loadable(() => import('@pages/Login'));
 const SignUp = loadable(() => import('@pages/SignUp'));
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/" element={<Navigate to="/login" />} />
-    </Routes>
-  );
+  const { isLoading, error, data } = useQuery('user', () => fetcher({ url: 'http://localhost:4000/api/users' }));
+
+  if (isLoading) return null;
+  if (error) return null;
+
+  if (data) {
+    return (
+      <Routes>
+        <Route path="/" element={<div>로그인 상태</div>} />
+        <Route path="*" element={<div>잘못된 접근</div>} />
+      </Routes>
+    );
+  } else {
+    return (
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    );
+  }
 };
 
 export default App;
